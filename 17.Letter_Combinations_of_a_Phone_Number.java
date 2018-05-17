0df434c82c90697e672e863252c633a1cb897df5
Suri Zhang <surizhang1224@gmail.com>
2018-05-17 10:24:59 -0400

    17, add my solution

--- 17.Letter_Combinations_of_a_Phone_Number.java
+++ 17.Letter_Combinations_of_a_Phone_Number.java
@@ -14,3 +14,33 @@ public List<String> letterCombinations(String digits) {
 		return ans;
 	}
 // Solution from discussion, using BFS
+
+// my solution 
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String[] mapping = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> comb = new ArrayList<>();
+        if(digits == null || digits.isEmpty()) {
+                return comb;
+            }
+        else {
+            comb.add("");
+            for(int i = 0; i < digits.length(); ++i){
+
+                for(int k = 0; k < comb.size(); ){
+                    String temp = comb.get(k);
+
+                    comb.remove(temp);
+
+                    int index = Integer.valueOf(digits.charAt(i) - '0');
+                    for(int j = 0; j < mapping[index].length(); ++j){
+
+                        comb.add(k, temp + mapping[index].charAt(j));
+                        ++k;
+                    }
+                }
+            }
+            return comb;
+        }
+    }
+}
